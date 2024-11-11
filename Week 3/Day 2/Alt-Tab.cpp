3b930f2b76cda242161d863a155ad2e18f4bc3c4
Sifat-00 <sifatbindoha123@gmail.com>
2024-11-11 09:41:49 -0800

    Add files via upload

---
+++ Week 3/Day 2/Alt-Tab.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    string ans;
+    vector<string> v;
+    while(t--)
+    {
+        string ar;
+        cin >> ar;
+        v.push_back(ar);
+    }   
+    reverse(v.begin(),v.end());
+    unordered_set<string> mp;
+    vector<string> track;
+    for(auto x : v)
+    {
+        if(mp.find(x) == mp.end())
+        {
+            track.push_back(x);
+            mp.insert(x);
+        }
+    }
+    for(auto x : track)
+    {
+        ans+=x.substr(x.size()-2);
+    }
+    //reverse(ans.begin(),ans.end());
+    cout << ans << endl;
+    return 0;
+}
